3d029f5480e94e98aa2bef9d7963dec66fd3f4df
Rudi Strahl <Rudi.Strahl@pointclickcare.com>
2015-10-25 11:31:17 -0400

    Fixed file loading, can now examine metadata for DCM file.

--- fileLoader.js
+++ fileLoader.js
@@ -1,6 +1,9 @@
 var dicomjs = require('dicomjs');
 
+document.body.addEventListener('load', initDropZone(document.getElementById('dropZone')));
+
 function initDropZone(element) {
+	console.log("Called initDropZone");
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
     dropZone.addEventListener('dragover', handleDragOver, false);
     dropZone.addEventListener('drop', handleFileSelect, false);
@@ -25,16 +28,39 @@ function handleDragOver(event) {
 }
 
 function loadFile(file) {
-	// do the love
-	dicomjs.parseFile(sampleDcmPath, function (err, dcmData) {
-	    console.log('Parsing file complete..');
-
-	    if (!err) {
-	        /// Reading patient name
-	        var patientName = dcmData.dataset['00100010'].value;
+	console.log("loadFile");
+	var reader = new FileReader();
+	reader.onload = function(file) {
+		console.log("onload called");
+		var arrayBuffer = reader.result;
+		var buffer = toBuffer(arrayBuffer);
+		dicomjs.parse(buffer, function (err, dcmData) {
+			if (!err) {
+					/// Reading patient name
+					var patientName = dcmData.dataset['00100010'].value;
+					var photometricInterpolation = dcmData.dataset['00280004'].value;
+					// var numberOfFrames = dcmData.dataset['00280008'].value;
+					var rows = dcmData.dataset['00280010'].value;
+					var columns = dcmData.dataset['00280011'].value;
 					console.log("Read patient record: "+patientName);
-	    } else {
-	        console.log(err);
-	    }
-	});
+					console.log("Read photometricInterpolation: "+photometricInterpolation);
+					// console.log("Read numberOfFrames: "+numberOfFrames);
+					console.log("Read rows: "+rows);
+					console.log("Read columns: "+columns);
+			} else {
+					console.log(err);
+			}
+		});
+	};
+	reader.readAsArrayBuffer(file);
+}
+
+// Refactor this out to actually translate an entire File object
+function toBuffer(arrayBuffer) {
+    var buffer = new Buffer(arrayBuffer.byteLength);
+    var view = new Uint8Array(arrayBuffer);
+    for (var i = 0; i < buffer.length; ++i) {
+        buffer[i] = view[i];
+    }
+    return buffer;
 }
