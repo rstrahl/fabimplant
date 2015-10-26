5cf4520fc64e620eaef2f44d50306bc44fa2447a
Jason Miller <jason@developit.ca>
2015-10-25 21:40:43 -0400

    Update to use ES6 ;)

--- fileLoader.js
+++ fileLoader.js
@@ -1,12 +1,14 @@
-var dicomParser = require('dicom-parser');
+import dicomParser from 'dicom-parser';
 
-document.body.addEventListener('load', initDropZone(document.getElementById('dropZone')));
+document.body.addEventListener('load', () => {
+	initDropZone(document.getElementById('dropZone'));
+});
 
 function initDropZone(element) {
 	console.log("Called initDropZone");
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
-    dropZone.addEventListener('dragover', handleDragOver, false);
-    dropZone.addEventListener('drop', handleFileSelect, false);
+		element.addEventListener('dragover', handleDragOver, false);
+		element.addEventListener('drop', handleFileSelect, false);
 	}
 	else {
 		alert("File APIs are not supported in this browser.");
@@ -29,17 +31,18 @@ function handleDragOver(event) {
 function loadFile(file) {
 	console.log("loadFile");
 	var reader = new FileReader();
-	reader.onload = function(file) {
+	reader.onload = file => {
 		console.log("onload called");
 		var arrayBuffer = reader.result;
 		var byteArray = new Uint8Array(arrayBuffer);
 		var dataSet = dicomParser.parseDicom(byteArray);
 
-    // get the pixel data element (contains the offset and length of the data)
-    var pixelDataElement = dataSet.elements.x7fe00010;
+		// get the pixel data element (contains the offset and length of the data)
+		var pixelDataElement = dataSet.elements.x7fe00010;
 		console.log("pixelDataElement: "+ JSON.stringify(pixelDataElement, null, '  '));
-    // create a typed array on the pixel data (this example assumes 16 bit unsigned data)
-    var pixelData = new Uint8Array(dataSet.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length);
+
+		// create a typed array on the pixel data (this example assumes 16 bit unsigned data)
+		var pixelData = new Uint8Array(dataSet.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length);
 		console.log(pixelData.slice(0,100));
 	};
 	reader.readAsArrayBuffer(file);
@@ -47,10 +50,10 @@ function loadFile(file) {
 
 // Refactor this out to actually translate an entire File object
 function toBuffer(arrayBuffer) {
-    var buffer = new Buffer(arrayBuffer.byteLength);
-    var view = new Uint8Array(arrayBuffer);
-    for (var i = 0; i < buffer.length; ++i) {
-        buffer[i] = view[i];
-    }
-    return buffer;
+	var buffer = new Buffer(arrayBuffer.byteLength);
+	var view = new Uint8Array(arrayBuffer);
+	for (var i = 0; i < buffer.length; ++i) {
+		buffer[i] = view[i];
+	}
+	return buffer;
 }
