311d78f18ac4212b207f6194c6700508a527c26b
Rudi Strahl <Rudi.Strahl@pointclickcare.com>
2015-11-02 21:40:52 -0500

    Integrated support for jpeg lossless compression transfer syntax, successful parse and display of CT!

--- fileLoader.js
+++ fileLoader.js
@@ -1,6 +1,10 @@
 import dicomParser from 'dicom-parser';
+import dataDictionary from './dataDictionary';
+import uids from './uids';
+import Decoder from 'JPEGLosslessDecoderJS';
 
-document.body.addEventListener('load', () => {
+window.addEventListener('load', () => {
+	console.log("Adding event listener.");
 	initDropZone(document.getElementById('dropZone'));
 });
 
@@ -41,14 +45,73 @@ function loadFile(file) {
 		var pixelDataElement = dataSet.elements.x7fe00010;
 		console.log("pixelDataElement: "+ JSON.stringify(pixelDataElement, null, '  '));
 
-		// create a typed array on the pixel data (this example assumes 16 bit unsigned data)
-		var pixelData = new Uint8Array(dataSet.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length);
-		console.log(pixelData.slice(0,100));
+		// TODO: refactor this out to some sort of helper struct
+		var imageMetadata = {
+			rows : dataSet.uint16('x00280010'),
+			cols : dataSet.uint16('x00280011'),
+			samplesPerPixel: dataSet.uint16('x00280002'),
+			pixelRepresentation : dataSet.uint16('x00280103'),
+			interpretation : dataSet.string('x00280004'),
+			planarConfiguration : dataSet.uint16('x00280006'),
+			frames : dataSet.uint16('x00280008'),
+			bitsAllocated : dataSet.uint16('x00280100'),
+			bitsStored : dataSet.uint16('x00280101'),
+			transferSyntax : uids[dataSet.string('x00020010')],
+			sopClassUID : uids[dataSet.string('x00080016')],
+			compressionMethod : dataSet.string('x00282114'),
+			pixelPaddingValue : dataSet.string('x00280120')
+		};
+
+		console.log("imageMetadata: "+ JSON.stringify(imageMetadata, null, '  '));
+
+    var canvas = document.querySelector('canvas'),
+		    ctx = canvas.getContext('2d'),
+		    width = imageMetadata.cols,
+				height = imageMetadata.rows;
+
+		// resize canvas to match dims
+		canvas.setAttribute('width', width);
+		canvas.setAttribute('height', height);
+		canvas.style.width = width + 'px';
+		canvas.style.height = height + 'px';
+
+		// decode image data
+		var byteOutput = imageMetadata.bitsAllocated <= 8 ? 1 : 2;
+		var decoder = new Decoder();
+		var decompressedData = decoder.decompress(dataSet.byteArray.buffer, pixelDataElement.fragments[0].position, pixelDataElement.fragments[0].length);
+		var pixelData;
+		if (imageMetadata.pixelRepresentation === 0) {
+      if (byteOutput === 2) {
+        pixelData = new Uint16Array(decompressedData);
+      } else {
+        // untested!
+        pixelData = new Uint8Array(decompressedData);
+      }
+    } else {
+      pixelData = new Int16Array(decompressedData);
+    }
+
+		// create imagedata
+		let img = ctx.getImageData(0,0,width, height);
+		let data = img.data;
+
+		let i = 0, j = 0;
+		while (i < pixelData.length) {
+			let val = (pixelData[i] / 4096) * 255; // normalized to 8bit
+			j = i*4;
+			data[j] = val;		// r
+		  data[j+1] = val;	// g
+		  data[j+2] = val;	// b
+		  data[j+3] = 255;	// a
+			i++;
+		}
+
+		ctx.putImageData(img, 0, 0);
 	};
 	reader.readAsArrayBuffer(file);
 }
 
-// Refactor this out to actually translate an entire File object
+// TODO: Refactor this out to actually translate an entire File object?
 function toBuffer(arrayBuffer) {
 	var buffer = new Buffer(arrayBuffer.byteLength);
 	var view = new Uint8Array(arrayBuffer);
