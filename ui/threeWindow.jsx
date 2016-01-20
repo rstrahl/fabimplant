7de3c56b626ff1954ccef60d0c247fb237e9b4ba
Rudi Strahl <strahlr@gmail.com>
2016-01-20 11:21:26 -0500

    Implement dynamic height/width and orthographic camera into ThreeWindow.

--- ui/threeWindow.jsx
+++ ui/threeWindow.jsx
@@ -4,7 +4,11 @@
 
 import React from 'react';
 import ReactTHREE from 'react-three';
+import { findDOMNode } from 'react-dom';
 import THREE from 'three';
+
+const NEAR = 1;
+const FAR = 5000;
 
 /**
  * Displays a threejs scene inside a window component.
@@ -13,30 +17,63 @@ export default class ThreeWindow extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			width : 0,
+			height : 0
+		};
+	}
+
+	componentDidMount() {
+		addEventListener('resize', this.updateSize);
+		this.updateSize();
+	}
+
+	componentWillUnmount() {
+		removeEventListener('resize', this.updateSize);
+	}
+
+	updateSize() {
+		this.setState({
+			width: findDOMNode(this).offsetWidth,
+			height: findDOMNode(this).offsetHeight
+		});
 	}
 
 	render() {
-		let width = 640;
-		let height = 480;
+		let { Scene, Mesh, OrthographicCamera } = ReactTHREE;
+		let { Vector3 } = THREE;
+		let { width, height } = this.state;
+
 		let aspectratio = width / height;
-		let cameraprops = {
+		let perspectiveCameraProps = {
 			fov : 75,
 			aspect : aspectratio,
-			near : 1, far : 5000,
-			position : new THREE.Vector3(0,0,600),
-			lookat : new THREE.Vector3(0,0,0)
+			near : NEAR,
+			far : FAR,
+			position : new Vector3(0,0,600),
+			lookat : new Vector3(0,0,0)
+		};
+		let orthoCameraProps = {
+			left : width / -2,
+			right: width / 2,
+			top : height / 2,
+			bottom : height / -2,
+			near : -500,
+			far : FAR
 		};
 
-		let position = new THREE.Vector3(0,0,0);
-		let geometry = new THREE.BoxGeometry(100,100,100);
-		let material = new THREE.MeshBasicMaterial( {color: 0x0000ff});
+		let meshProps = {
+			position : new THREE.Vector3(0,0,0),
+			geometry : new THREE.BoxGeometry(100,100,100),
+			material : new THREE.MeshBasicMaterial( {color: 0x0000ff} )
+		};
 
 		return (
 			<div className="three-window">
-		        <ReactTHREE.Scene width={width} height={height} camera="maincamera">
-		            <ReactTHREE.PerspectiveCamera name="maincamera" {...cameraprops} />
-					<ReactTHREE.Mesh position={position} geometry={geometry} material={material} />
-		        </ReactTHREE.Scene>
+		        <Scene camera="maincamera" width={width} height={height}>
+		            <OrthographicCamera name="maincamera" {...orthoCameraProps} />
+					<Mesh {...meshProps} />
+		        </Scene>
 			</div>
 		);
 	}
